e6b0c41e59cb7babef4f967fc608224f74e15c40
Alexandre Lachèze <alexandre.lacheze@gmail.com>
2016-06-03 20:30:03 +0200

    Use tornado.queues.Queue instead

--- app/handlers/events.py
+++ app/handlers/events.py
@@ -1,47 +1,20 @@
 from tornado import gen
 from tornado.iostream import StreamClosedError
-from tornado.concurrent import Future
+from tornado.queues import Queue
 
 from base import BaseHandler
-
-class DataBuffer(object):
-    def __init__(self, ee):
-        self.ee = ee
-        self.ee.on('event', self.new_data)
-
-        self.waiters = set()
-
-    def wait_for_data(self):
-        # Construct a Future to return to our caller.  This allows
-        # wait_for_messages to be yielded from a coroutine even though
-        # it is not a coroutine itself.  We will set the result of the
-        # Future when results are available.
-        result_future = Future()
-        self.waiters.add(result_future)
-        return result_future
-
-    def cancel_wait(self, future):
-        self.waiters.remove(future)
-        # Set an empty result to unblock any coroutines waiting.
-        future.set_result([])
-
-    def new_data(self, data):
-        for future in self.waiters:
-            future.set_result(data)
-        self.waiters = set()
-
-    def close(self):
-        for future in self.waiters.copy():
-            self.cancel_wait(future)
-        self.ee.remove_listener('event', self.new_data)
 
 class EventsHandler(BaseHandler):
 
     def initialize(self):
-        self.buffer = DataBuffer(self.db_ee)
+        self.data_queue = Queue()
+        self.db_ee.on('event', self.add_to_data_queue)
 
         self.set_header('content-type', 'text/event-stream')
         self.set_header('cache-control', 'no-cache')
+
+    def add_to_data_queue(self, data):
+        self.data_queue.put(data)
 
     @gen.coroutine
     def publish(self, data):
@@ -52,13 +25,12 @@ class EventsHandler(BaseHandler):
             pass
 
     def on_connection_close(self):
-        self.buffer.close()
+        self.db_ee.remove_listener('event', self.add_to_data_queue)
 
     @gen.coroutine
     def get(self):
         while not self.request.connection.stream.closed():
-            self.future = self.buffer.wait_for_data()
-            data = yield self.future
+            data = yield self.data_queue.get()
             yield self.publish(data)
 
 default_handlers = [
