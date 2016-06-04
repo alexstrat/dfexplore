94158dcf87963f5faf4578cc959ecb856486ecaa
Alexandre Lachèze <alexandre.lacheze@gmail.com>
2016-06-04 13:06:03 +0200

    Add post and get routes for workspace

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,10 +1,12 @@
 import hello
 import dataframe
 import events
+import workspace
 import static
 
 def get_all():
     return hello.default_handlers + \
         dataframe.default_handlers + \
         events.default_handlers +\
+        workspace.default_handlers +\
         static.default_handlers
---
+++ app/handlers/workspace.py
@@ -0,0 +1,34 @@
+import tornado.web
+
+from base import BaseHandler
+from ..models import Workspace
+
+class WorkspaceHandler(BaseHandler):
+    def get(self):
+        workspaces = self.db.query(Workspace)
+
+        query_name = self.get_query_argument('name', False)
+        if query_name:
+            workspaces = workspaces.filter(Workspace.name == query_name)
+
+        response = [{
+            'id': ws.id,
+            'name': ws.name
+        } for ws in workspaces]
+
+        self.write_json(response)
+
+    def post(self):
+        name = self.get_body_argument('name', False)
+        if not name:
+            raise tornado.web.HTTPError(400, 'missing name in body')
+
+        ws = Workspace(name=name)
+        self.db.add(ws)
+        self.db.commit()
+
+        self.write_json({'id': ws.id})
+
+default_handlers = [
+    (r"/workspace/?", WorkspaceHandler),
+]
