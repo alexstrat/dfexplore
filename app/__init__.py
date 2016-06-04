4ef428642e7010e32e97b0724e7415a815501a45
Alexandre Lachèze <alexandre.lacheze@gmail.com>
2016-06-04 12:52:46 +0200

    Add debug to unlock autorelaod

--- app/__init__.py
+++ app/__init__.py
@@ -25,7 +25,11 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = app_handlers.get_all()
 
-        tornado.web.Application.__init__(self, handlers, template_path=TEMPLATE_PATH)
+        tornado.web.Application.__init__(self,
+            handlers,
+            template_path = TEMPLATE_PATH,
+            debug = config.DEBUG
+        )
 
         engine = create_engine(config.SQLALCHEMY_DATABASE_URI, convert_unicode=True, echo=config.DEBUG)
         models.init_db(engine)
